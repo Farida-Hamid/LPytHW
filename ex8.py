72de0a2f132073689c5aff1a247e293d13ddea3d
Farida-Hamid <faridaaddala@gmail.com>
2023-12-18 16:13:20 +0300

    Add ex7

---
+++ ex8.py
@@ -0,0 +1,7 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "tow", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format("She's a", "Barbie girl", "in a", "Barbie world."))
