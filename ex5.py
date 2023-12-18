5dc49fca0eb6b09c40edc1f140bf6d743d62f93c
Farida-Hamid <faridaaddala@gmail.com>
2023-12-18 14:14:03 +0300

    Modify ex5: add rounding

--- ex5.py
+++ ex5.py
@@ -7,8 +7,8 @@ teeth = 'White'
 hair = 'Brown'
 
 print(f"Let's talk anbout {name}.")
-print(f"He's {height} inches tall, {height * 0.0254} meters tall.")
-print(f"He's {weight} poubds heavy, {weight * 0.45} Kg heavy.")
+print(f"He's {height} inches tall, {round(height * 0.0254, 2)} meters tall.")
+print(f"He's {weight} poubds heavy, {round(weight * 0.45)} Kg heavy.")
 print("Actually  that's not too heavy")
 print(f"He's got {eyes} eyes and {hair} hair.")
 print(f"His teeth are usually {teeth} depending on the coffe.")
