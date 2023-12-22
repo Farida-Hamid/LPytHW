d0d2bad69eb358ff4f6aa6c93d89391617145294
Farida-Hamid <faridaaddala@gmail.com>
2023-12-22 14:41:43 +0300

    Modify ex13

--- ex13.py
+++ ex13.py
@@ -2,10 +2,14 @@ from sys import argv
 
 script, first, second, third = argv
 
+ip = ""
+ip = input("One more thing ")
+
 print("The sctipt is called:", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
+print("You added ", ip)
 
 # This program is ran by psiing arguments as:
 # python ex13.py one two three
