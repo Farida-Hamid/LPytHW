0e718d9735a07646332453e8754f394d4b80aeb5
Farida-Hamid <faridaaddala@gmail.com>
2023-12-22 14:36:06 +0300

    Add ex13

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The sctipt is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+# This program is ran by psiing arguments as:
+# python ex13.py one two three
