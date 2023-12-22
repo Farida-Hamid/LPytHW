7724038c5a750cd934c24bd9b64d4cbf158e5da7
Farida-Hamid <faridaaddala@gmail.com>
2023-12-22 14:59:39 +0300

    Modify ex14

--- ex14.py
+++ ex14.py
@@ -1,12 +1,15 @@
 from sys import argv
 
 script, user_name = argv
-prompt = '> '
+prompt = '==> '
 
 print(f"Hi, {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
+
+print("How old are you?")
+age = input(prompt)
 
 print(f"Where do you live {user_name}?")
 lives = input(prompt)
@@ -17,5 +20,6 @@ computer = input(prompt)
 print(f"""
 Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
+You are {age} years old.
 And you have a {computer} computer. Nice.
       """)
