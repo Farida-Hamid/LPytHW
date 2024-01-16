9cff052c8090e3d28fd6c0ec0e65d8ee3230b380
Farida-Hamid <faridaaddala@gmail.com>
2024-01-16 17:14:40 +0300

    Modify ex33

--- ex33.py
+++ ex33.py
@@ -1,16 +1,19 @@
-i = 0
-numbers = []
+def looper (untill):
+  i = 0
+  numbers = []
 
-while i < 6:
-  print(f"At the top is {i}")
-  numbers.append(i)
+  while i < untill:
+    print(f"At the top is {i}")
+    numbers.append(i)
 
-  i = i + 1
+    i += 1
 
-  print("Numbwrs now: ", numbers)
-  print(f"At the botto, is {i}")
+    print("Numbwrs now: ", numbers)
+    print(f"At the bottom, is {i}")
 
-print("The numbers: ")
+  print("The numbers: ")
 
-for num in numbers:
-  print(num)
+  for num in numbers:
+    print(num)
+
+looper(4)
