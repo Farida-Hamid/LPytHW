e00990cd4f198e9e0394fd8d7f7f98daafcc57ec
Farida-Hamid <faridaaddala@gmail.com>
2024-01-29 20:25:22 +0300

    Add laser weapon armory class to ex43

--- ex43_classes.py
+++ ex43_classes.py
@@ -62,7 +62,7 @@ class CentralCorridor(Scene):
     if action == "shoot!":
       print(dedent("""
             The Quick on the draw you yank out your blaster and fire
-            iit at the Gothon. His clown costume is flowing and
+            it at the Gothon. His clown costume is flowing and
             moving his nody, which thoughs off your aim.
             Your laser hits his costume but misses him entirely.
             this complerely ruins his brand new costume  his mother 
@@ -103,7 +103,42 @@ class CentralCorridor(Scene):
 class LaserWeaponArmory(Scene):
 
   def enter(self):
-    pass
+    print(dedent("""
+        You do a drive roll into the Weapon Armory, crouch and scan
+        the room for more Gothons that might be hidden. It's dead
+        qiet, too qiet. You stand up and run to the far side of
+        the room and find the neutron bomb in its container.
+        there's a keypad lock on the box and you need the code to
+        get the bomb out. If you get the code wrong 10 times then
+        the lock closes forever and you can't get the bomb. The
+        code is 3 digits.
+        """))
+    
+    code = f"{randint(1,9)}{randint(1,9)}{randint(1,9)}" 
+    guess = input("[Keypad]> ")
+    guesses = 0
+
+    while guess != code and guesses < 10:
+      print("BUZZZZED!")
+      guesses += 1
+      guess = input("[keypad]> ")
+
+    if guess == code:
+      print(dedent("""
+            The container clicks open and the seal breaks, letting
+            gas out. You grab the neutron bomb and run as fast as
+            you can to the bridge where you must place it in the
+            right spot.
+            """))
+      return 'the_bridge'
+    else:
+      print(dedent("""
+                  The lock buzzes one last time and the you hear a
+                  sickening melting sound as the mechanism is fused
+                  together. You decide to sit there, and finally the
+                  Gothons blow uo the ship from their ship and you die.
+                  """))
+      return 'death'
 
 class TheBridge(Scene):
 
