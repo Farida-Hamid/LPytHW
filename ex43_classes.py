2c40dbfe101f6f0f0d4efd7494854603c5b7883b
Farida-Hamid <faridaaddala@gmail.com>
2024-01-29 19:26:19 +0300

    Add scene and engine classes to ex43

--- ex43_classes.py
+++ ex43_classes.py
@@ -1,16 +1,30 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
+
 class Scene(object):
 
   def enter(self):
-    pass
+    print("This scene is not yet configured.")
+    print("sSubclass it and implement enter().")
+    exit(1)
 
 
 class Engine(object):
 
-  def __init__(self):
-    pass
+  def __init__(self, scene_map):
+    self.scene_map = scene_map
 
   def play(self):
-    pass
+    current_scene = self.scene_map.opening_scene()
+    last_scenen = self.scene_map.next_scense('finished')
+
+    while current_scene != last_scenen:
+      next_scene_name = current_scene.enter()
+      current_scene = self.scene_map.next_scene(next_scene_name)
+
+    # be sure to print out the last scenen
+    current_scene.enter()
 
 class Death(Scene):
 
